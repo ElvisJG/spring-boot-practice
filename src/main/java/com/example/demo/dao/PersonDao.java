4bc184fc0156c0d3a029fb0c17aeb6c046712a94
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-10-14 00:51:43 -0400

    Full CRUD on person object

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+    // mock database using a list, returns 0 or 1 depending on if data is persisted
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePerson(UUID id, Person person);
+}
